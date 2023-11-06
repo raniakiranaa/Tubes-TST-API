728021b9205beaf5b31cc56f5de45f6d6c3564e1
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-11-06 19:06:36 +0700

    add changes, fix bug

--- smartcart.py
+++ smartcart.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, HTTPException, Query
 from typing import Optional
 import mysql.connector
 from mysql.connector import errorcode
@@ -25,22 +25,24 @@ except mysql.connector.Error as err:
 	else:
 		print(err)
 else:
-	cursor = conn.cursor()
-
 	app = FastAPI()
    
 	@app.get('/product')
 	async def read_all_product():
+		cursor = conn.cursor()
 		query = ("SELECT * FROM product")
 		cursor.execute(query)
 		result = cursor.fetchall()
+		cursor.close()
 		return result
 	
 	@app.get('/product/{name_product}')
 	async def search_info_product(name_product: str):
+		cursor = conn.cursor()
 		query = f"SELECT * FROM product join aisle on product.id_aisle = aisle.id WHERE name_product = '{name_product}'"
 		cursor.execute(query)
 		result = cursor.fetchall()
+		cursor.close()
 
 		if result:
 			aisle_name = result[0][8]
@@ -56,81 +58,96 @@ else:
 			
 			return response
 		else:
-			return f"No information found for product: {name_product}"
-	
-	@app.get('/product/{name_category}')
-	async def search_info_category(name_category: str):
-		query = f"SELECT DISTINCT name_product FROM product join category on product.id_category = category.id WHERE category.name = '{name_category}'"
-		cursor.execute(query)
-		result = cursor.fetchall()
-
-		if result:
-			count = len(result)
-			products = ', '.join(item[0] for item in result)
-			if count == 1:
-				return f"Product available for category {name_category} is: {products}"
-			else:
-				return f"Products available for category {name_category} are: {products}"
-		else:
-			return f"No information found for product available in category: {name_category}"
+			raise HTTPException(status_code=404, detail=f"No information found for product: {name_product}")
 
 	@app.get('/aisle')
 	async def read_all_aisle():
+		cursor = conn.cursor()
 		query = ("SELECT * FROM aisle")
 		cursor.execute(query)
 		result = cursor.fetchall()
+		cursor.close()
 		return result
 
 	@app.get('/category')
 	async def read_all_category():
+		cursor = conn.cursor()
 		query = ("SELECT * FROM category")
 		cursor.execute(query)
 		result = cursor.fetchall()
+		cursor.close()
 		return result
 	
-	# @app.get('/transaction/{id_product}')
-	# async def product_recommendation(id_product: int):
-	# 	query = "SELECT id, id_product FROM transaction JOIN detail_transaction ON transaction.id = detail_transaction.id_transaction"
-	# 	cursor.execute(query)
-	# 	result = cursor.fetchall()
-
-	# 	transaction_data = defaultdict(list)
-	# 	for item in result:
-	# 		id_transaction, id_item = item
-	# 		transaction_data[id_transaction].append(id_item)
-		
-	# 	result = [tuple(trans) for trans in transaction_data.values()]
-		# BELOMMMMM P
-		
 	@app.get('/detail_transaction/')
 	async def read_fav_merk(name_product: Optional[str] = Query(None)):
+		cursor = conn.cursor()
 		if name_product:
 			query = f"SELECT name_product, merk, SUM(quantity) as total FROM detail_transaction JOIN product ON detail_transaction.id_product = product.id WHERE name_product = '{name_product}' GROUP BY name_product, merk ORDER BY total DESC"
 			cursor.execute(query)
 			result = cursor.fetchall()
+			cursor.close()
 
 			if result:
 				fav_merk = result[0][1]
 				return f"The most bought merk for product {name_product} is {fav_merk}"
 			else:
-				return f"No transaction found for product: {name_product}"
+				raise HTTPException(status_code=404, detail=f"No transaction found for product: {name_product}")
 		else:
 			query = ("SELECT * FROM detail_transaction")
 			cursor.execute(query)
 			result = cursor.fetchall()
+			cursor.close()
 			return result
-		
-	@app.get('/detail_transaction/{id_transaction}')
-	async def read_detail_transaction(id_transaction: int):
-		query = f"SELECT * FROM detail_transaction where id_transaction = {id_transaction}"
-		cursor.execute(query)
-		result = cursor.fetchall()
-		return result
+
+	@app.get('/detail_transaction/{id_product}')
+	async def product_recommendation(id_product: int):
+		cursor = conn.cursor()
+		query = "SELECT name_product FROM product WHERE id = %s"
+		cursor.execute(query, (id_product,))
+		name = cursor.fetchone()
+		if name:
+			query = "SELECT id_transaction, id_product FROM detail_transaction"
+			cursor.execute(query)
+			result = cursor.fetchall()
+
+			transaction_data = defaultdict(list)
+			for item in result:
+				id_transaction, id_item = item
+				transaction_data[id_transaction].append(id_item)
+			
+			result = [tuple(trans) for trans in transaction_data.values()]
+			related_transactions = [transaction for transaction in result if id_product in transaction]
+			items = [item for transaction in related_transactions for item in transaction if item != id_product]
+
+			counter = Counter(items)
+			most_common = counter.most_common()
+			list_item = []
+			for item in most_common:
+				query = "SELECT name_product FROM product WHERE id = %s"
+				cursor.execute(query, (item[0],))
+				item_name = cursor.fetchone()
+				if item_name and item_name != name:
+					list_item.append(item_name)
+
+			cursor.close()
+
+			list_frequent = list(set(list_item))[:3]
+			list_new = [item[0] for item in list_frequent]
+			if list_new:
+				return f"Product frequently bought with {name[0]}: {list_new}"
+			else:
+				return "We have no information on this. Be the first to try it out!"
+			
+		else:
+			cursor.close()
+			raise HTTPException(status_code=404, detail=f"No information found for product with ID = {id_product}")
 
 	def check_id_exists(table_name: str, id: int):
+		cursor = conn.cursor()
 		query = f"SELECT * FROM {table_name} WHERE id = {id}"
 		cursor.execute(query)
 		result = cursor.fetchall()
+		cursor.close()
 		return bool(result)
 
 	@app.post('/product')
@@ -139,15 +156,18 @@ else:
 		category_exists = check_id_exists('category', id_category)
 
 		if aisle_exists and category_exists and stock > 0 and price > 0:
+			cursor = conn.cursor()
 			query = f"INSERT INTO product (name_product, price, id_category, merk, stock, id_aisle) VALUES ('{name_product}', {price}, {id_category}, '{merk}', {stock}, {id_aisle})"
 			cursor.execute(query)
 			conn.commit()
+			cursor.close()
 			return f"Product {name_product} has been added"
 		else:
-			return f"Data is not valid, product {name_product} has not been added"
+			raise HTTPException(status_code=422, detail=f"Data is not valid, product {name_product} has not been added")
 
 	@app.put('/product')
 	async def update_product(id_product: int, price = int, stock = int):
+		cursor = conn.cursor()
 		query = f"SELECT * FROM product WHERE id={id_product}"
 		cursor.execute(query)
 		result = cursor.fetchall()
@@ -155,12 +175,15 @@ else:
 			query = f"UPDATE product SET stock = {stock}, price = {price} WHERE id = {id_product}"
 			cursor.execute(query)
 			conn.commit()
+			cursor.close()
 			return f"Stock for id_product = {id_product} is updated"
 		else:
-			return f"No product found with id = {id_product}"
+			cursor.close()
+			raise HTTPException(status_code=404, detail=f"No product found with id = {id_product}")
 		
 	@app.delete('/product/{id}')
 	async def delete_product(id: int):
+		cursor = conn.cursor()
 		query = f"SELECT * FROM product WHERE id = {id}"
 		cursor.execute(query)
 		result = cursor.fetchall()
@@ -168,6 +191,8 @@ else:
 			query = f"DELETE FROM product WHERE id = {id}"
 			cursor.execute(query)
 			conn.commit()
+			cursor.close()
 			return f"Product with id_product = {id} is deleted"
 		else:
-			return f"No product found with id = {id}"
+			cursor.close()
+			raise HTTPException(status_code=404, detail=f"No product found with id = {id}")
